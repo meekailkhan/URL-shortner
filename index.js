366993b3204f2b7499e95353395fc4cd6244f082
pc <meekailkhan123aar@gmail.com>
2024-12-04 22:50:38 +0530

    url shortner

--- index.js
+++ index.js
@@ -14,14 +14,6 @@ connection('mongodb://127.0.0.1:27017/shortUrl').then(()=> console.log("connecti
 
 app.use('/url',routes)
 
-app.get('/test' ,async(req,res)=>{
-    const allUrls = await db.URL.find({});
-    return res.end( `
-    <ul>
-        ${allUrls.map(url => `<li> ${url.shortId}, ${url.redirectURL} - ${url.visitHistory.length}</li>`).join("")}
-    </ul>
-    `)
-    // return res.end(html)
-})
+
 
 app.listen(PORT,()=> console.log(`listen port at http://localhost:${PORT}`));
